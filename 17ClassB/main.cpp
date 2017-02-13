d9d59aec95f04be743cf86cfc41ba769cfebdddd
Jonathon Tupper <Jonathon Tupper>
2017-02-12 18:46:21 -0800

    new main.cpp code

--- 17ClassB/main.cpp
+++ 17ClassB/main.cpp
@@ -4,6 +4,16 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    char answer = '';
+    cout << "Hello Hub!" << endl;
+    cout << "are you ready for calculations?(y,n)" << endl;
+    cin >> answer;
+    if (answer == 'n')
+    cout << "Thats about all I do." << endl;
+    else if (answer == 'y')
+        cout << "I can do simple mathematics.(add, sub, mult, divs)" << endl;
+        cout << "enter your simple math problem" << endl;
+        cin >> >> endl;
+
     return 0;
 }
